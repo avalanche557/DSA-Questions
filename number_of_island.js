e8379e67076fee53b38fb632498be2f49c367d82
avalanche557 <abhinavkarn55@gmail.com>
2024-03-21 01:32:15 +0530

    21th March, 24

--- number_of_island.js
+++ number_of_island.js
@@ -1,35 +1,29 @@
-var numIslands = function(grid, connectedComponents = 0) {
-    const [ rows, cols ] = [ grid.length, grid[0].length ]
-
-    for (let row = 0; row < rows; row++) {/* Time O(ROWS) */
-        for (let col = 0; col < cols; col++) {/* Time O(COLS) */
-            const isIsland = grid[row][col] === '1';
-            if (isIsland) connectedComponents++;
-
-            bfs(grid, rows, cols, new Queue([ [ row, col ] ]));/* Space O(MIN(ROWS,COLS)) */
-        }
-    }
-
-    return connectedComponents
- }
-
- const bfs = (grid, rows, cols, queue) => {
-    while (!queue.isEmpty()) {
-        for (let i = (queue.size() - 1); 0 <= i; i--) {/* Time O(WIDTH) */
-            const [ row, col ] = queue.dequeue();
-
-            const isWater = grid[row][col] === '0';
-            if (isWater) continue;
-
-            grid[row][col] = '0';
-
-            for (const [ _row, _col ] of getNeighbors(row, rows, col, cols)) {
-                queue.enqueue([ _row, _col ]);             /* Space O(MIN(ROWS,COLS)) */
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    let count = 0
+    let rows = grid.length;
+    let columns = grid[0].length;
+    for(let r = 0 ; r< rows; r++) {
+        for(let c = 0; c < columns; c++) {
+            if(grid[r][c] === "1") {
+                count++
+                changeToWater(grid, r, c)
             }
         }
     }
- }
+    return count;
+};
 
-var getNeighbors = (row, rows, col, cols) => [ [ 0, 1 ], [ 0, -1 ], [ 1, 0 ], [ -1, 0 ] ]
-    .map(([ _row, _col]) => [ (row + _row), (col + _col) ])
-    .filter(([ _row, _col ]) => (0 <= _row) && (_row < rows) && (0 <= _col) && (_col < cols));
+const changeToWater = (grid, rowIndex, columnIndex) => {
+    if(grid[rowIndex] === undefined || grid[rowIndex][columnIndex] === undefined || grid[rowIndex][columnIndex]=== "0") {
+        return ;
+    }
+    grid[rowIndex][columnIndex] = '0';
+    changeToWater(grid, rowIndex+1, columnIndex)
+    changeToWater(grid, rowIndex-1, columnIndex)
+    changeToWater(grid, rowIndex, columnIndex+1)
+    changeToWater(grid, rowIndex, columnIndex-1)
+}
